35b7f0fa1252f4def62af34c97b4630681b43293
Debarchito Nath <dev.debarchito.nath@protonmail.com>
2024-09-14 19:50:55 +0530

    Format

--- frontend/.svelte-kit/generated/client/app.js
+++ frontend/.svelte-kit/generated/client/app.js
@@ -1,26 +1,28 @@
-export { matchers } from './matchers.js';
+export { matchers } from "./matchers.js";
 
 export const nodes = [
-	() => import('./nodes/0'),
-	() => import('./nodes/1'),
-	() => import('./nodes/2'),
-	() => import('./nodes/3'),
-	() => import('./nodes/4'),
-	() => import('./nodes/5')
+  () => import("./nodes/0"),
+  () => import("./nodes/1"),
+  () => import("./nodes/2"),
+  () => import("./nodes/3"),
+  () => import("./nodes/4"),
+  () => import("./nodes/5")
 ];
 
 export const server_loads = [];
 
 export const dictionary = {
-		"/(app)": [3],
-		"/(auth)/login": [~4,[2]],
-		"/(auth)/register": [5,[2]]
-	};
-
-export const hooks = {
-	handleError: (({ error }) => { console.error(error) }),
-
-	reroute: (() => {})
+  "/(app)": [3],
+  "/(auth)/login": [~4, [2]],
+  "/(auth)/register": [5, [2]]
 };
 
-export { default as root } from '../root.js';
+export const hooks = {
+  handleError: ({ error }) => {
+    console.error(error);
+  },
+
+  reroute: () => {}
+};
+
+export { default as root } from "../root.js";
